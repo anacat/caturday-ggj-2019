15df5c3eae0b03d0a7e218bf94ac58c6ebddb7da
Michael Matias <michael.f.matias@gmail.com>
2019-01-26 10:38:07 +0000

    Added scripts.

---
+++ Caturday/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+    public NetworkManager NetworkManager { get; private set; }
+
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(Initialize());
+    }
+
+    private IEnumerator Initialize()
+    {
+        yield return StartCoroutine(InitializeNetwork());
+    }
+    
+    private IEnumerator InitializeNetwork()
+    {
+        NetworkManager = new NetworkManager();
+        yield return null;
+    }
+}
---
+++ Caturday/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class NetworkManager
+{
+    private UdpClient _udpBroadcaster;
+    private IPEndPoint _broadcastIpEndPoint;
+    public NetworkManager()
+    {
+        InitializeBroadcast();
+    }
+
+    private void InitializeBroadcast()
+    {
+        _udpBroadcaster = new UdpClient();
+        _broadcastIpEndPoint = new IPEndPoint(IPAddress.Broadcast, 13947);
+
+    }
+
+    public void StartBroadcasting()
+    {
+        for (; ; )
+        {
+            //string message = "HELLO!";
+            //Byte[] buffer = new Byte[1000];
+            //buffer = ASCIIEncoding.ASCII.GetBytes(message);
+            //_udpBroadcaster.Send(buffer, buffer.Length, _broadcastIpEndPoint);
+        }
+    }
+}
