cd5af064ad229de9d93c2f601d9984c527166744
anacat <catarina.lopes.04@hotmail.com>
2019-01-26 19:29:27 +0000

    Apartment
    Also doors + basic script to open doors.
    Also first person controller.

---
+++ Assets/Scripts/Game/ObjectInteraction/OpenDoor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{    
+    private bool _rotate;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _rotate = true; 
+        }
+
+        if(_rotate)
+        {
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(new Vector3(-90, 0, 90)), Time.deltaTime);
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+
+        }
+    }
+}
