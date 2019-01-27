e96df55687c30cd2dbe2b0d377fb5423368d0c5d
anacat <catarina.lopes.04@hotmail.com>
2019-01-27 00:06:24 +0000

    Apartment doors
    working boyyyy

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool _interact;
+
+    private float _interactionCooldown = 2f;
+    private float _timer = 0;
+
+    private void Update()    
+    {
+        if(_timer >= _interactionCooldown) 
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                _interact = true;
+            }
+            else if(Input.GetKeyUp(KeyCode.E))
+            {
+                _interact = false;
+            }
+        }
+        else 
+        {
+            _timer += Time.deltaTime;
+            _interact = false;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(!_interact)
+        {
+            return;
+        }
+
+        if(other.CompareTag("Door"))
+        {
+            other.GetComponent<DoorOpener>().OpenCloseDoor();
+
+            _timer = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/ObjectInteraction/DoorOpener.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{    
+    private bool _rotate;
+    private Vector3 _rotationVector = new Vector3(0, 0, 90);
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _rotate = true; 
+        }
+
+        if(_rotate)
+        {
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(_rotationVector), Time.deltaTime);
+        }
+        else 
+        {
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(Vector3.zero), Time.deltaTime);
+        }
+    }
+
+    public void OpenCloseDoor()
+    {
+        if(_rotate)
+        {
+            _rotate = false;
+        }
+        else
+        {
+            _rotate = true;
+        }        
+    }
+}
